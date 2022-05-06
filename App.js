3337676a61d6c277c476300530cf239a6e8d8598
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-05-06 09:01:32 -0400

    Commit LP

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {Database} from "./database";
+import { FlatList, StyleSheet, Text, View, Pressable} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Fontisto } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useState } from 'react';
 
 const db = new Database("Shop");
+const Tab = createBottomTabNavigator();
 
 db.execute("drop table if exists produits;");
 db.execute("create table produits (id, nom, prix, image);");
@@ -14,6 +21,9 @@ db.execute("insert into Connexion values ('LPR' , '123456' , 1);");
 export default function App() {
   const [produits, setProduits] = useState([]);
   const [connexions , setConnexion] = useState([]);
+  const [userConnected , setUserConnected] = useState(false);
+  const [nomUser , setUser] = useState();
+  const [adminConnecter , setAdminConnecter] = useState(false);
   db.execute("select * from produits;")
   .then((resultSet) => {
       setProduits(resultSet.rows)
@@ -22,12 +32,66 @@ export default function App() {
     .then((resultSet) => {
         setConnexion(resultSet.rows)
     }).catch((m)=>{  setErreur("Erreur exec Select " + m);})
+
+
+   const AcceuilScreen = ({navigation}) => {return (<View>
+     <Text style={styles.title}>Magasin!</Text>
+     {userConnected ?<Pressable  style={styles.pressable} 
+    onPress={() => navigation.navigate("ProduitsScreen", {/*  */})}>
+      <Text style={styles.pressable_text}>Caroline</Text>
+    </Pressable>:null}
+    {adminConnecter ?<Pressable  style={styles.pressable} 
+    onPress={() => navigation.navigate("AdminScreen", {/*  */})}>
+      <Text style={styles.pressable_text}>Caroline</Text>
+    </Pressable>:null}
+   </View>)}
+    const AboutScreen = ({navigation}) => {return (<View>
+      <Text style={styles.title}>Magasin!</Text>
       
+    </View>)}
+    const FindUsScreen = ({navigation}) => {return (<View>
+      <Text style={styles.title}>Magasin!</Text>
+      
+    </View>)}
+    const AdminScreen = ({navigation}) => {return (<View>
+      <Text style={styles.title}>Magasin!</Text>
+      
+    </View>)}
+    const ProduitsScreen = ({navigation}) => {return (<View>
+      <Text style={styles.title}>Nos croisières en promotion!</Text>
+      <FlatList
+        data={produits}
+        renderItem={({item}) => /*Produits */ null}
+        keyExtractor={(item, index) => index.toString()}/>
+    </View>)}
+   const SearchScreen = () => <View><Text>Recherche</Text></View>
+   
+
+   const PressableLogin = ({user , motdepasse , flag }) => {
+     const [isPressed, setIsPressed] = useState(false);
+     return (<Pressable style={isPressed ? styles.appuye : styles.pressable}
+       onPressIn={() => setIsPressed(true)} onPressOut={() => setIsPressed(false)}>
+         <Text style={isPressed ? styles.texteAppuye : styles.texteNormal}>{user}{flag}</Text>
+         {setUser(user)} {flag == 1?setAdminConnecter(true):setAdminConnecter(false)}
+     </Pressable>
+     )};
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Accueil" component={AcceuilScreen} 
+            options={{tabBarIcon: ({size, focused}) =>
+            <Ionicons name="home" size={size} color={focused ? "blue" : "lightblue"} />}} />
+         
+          <Tab.Screen name="About" component={AboutScreen}
+            options={{tabBarIcon: ({size, focused}) =>
+            <Ionicons name="search" size={size} color={focused ? "blue" : "lightblue"} />}}/>
+
+          <Tab.Screen name="Find us" component={FindUsScreen}
+            options={{tabBarIcon: ({size, focused}) =>
+            <Fontisto name="ship" size={size} color={focused ? "blue" : "lightblue"} />}}/>
+        </Tab.Navigator>
+      </NavigationContainer>
   );
 }
   
