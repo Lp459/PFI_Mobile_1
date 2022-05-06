4b52066c306630db8ae45b1562e9914e9eb72065
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-05-06 08:23:26 -0400

    first edit

--- App.js
+++ App.js
@@ -1,7 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Database} from "./database";
+
+const db = new Database("Shop");
+
+db.execute("drop table if exists produits;");
+db.execute("create table produits (id, nom, prix, image);");
+db.execute("drop table if exists connexion;");
+db.execute("create table connexion (usager, motdepasse, admin);");
+db.execute("insert into produits values (1, 'Pain 800g' , 10 , image.jpg);");
+db.execute("insert into Connexion values ('LPR' , '123456' , 1);");
 
 export default function App() {
+  const [produits, setProduits] = useState([]);
+  const [connexions , setConnexion] = useState([]);
+  db.execute("select * from produits;")
+  .then((resultSet) => {
+      setProduits(resultSet.rows)
+  }).catch((m)=>{  setErreur("Erreur exec Select " + m);})
+    db.execute("select * from connexions;")
+    .then((resultSet) => {
+        setConnexion(resultSet.rows)
+    }).catch((m)=>{  setErreur("Erreur exec Select " + m);})
+      
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -9,7 +30,7 @@ export default function App() {
     </View>
   );
 }
-
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
