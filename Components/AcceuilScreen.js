1697f86e27b8f9a0ad45321e414516ec9dc02781
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-05-10 11:37:56 -0400

    AccueilScreen

--- Components/AcceuilScreen.js
+++ Components/AcceuilScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { View, StyleSheet , Text, Pressable } from 'react-native';
+import { View, StyleSheet , Text, Pressable, Image } from 'react-native';
 
-function AcceuilScreen() {
+function AccueilScreen({navigation, route}) {
+    const {userId, nom, admin} = route.params;
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Magasin!</Text>
+            <Text style={styles.title}>Bienvenue dans NewEgg!</Text>
+            <Image
+              style={styles.logo}
+              source={require('../assets/newEgg.jpg')}
+            />
             <Pressable  style={styles.pressable} 
-            onPress={() => navigation.navigate("LoginScreen", {/* */})}>
+            onPress={() => navigation.navigate("Login", {/* */})}>
             <Text style={styles.pressable_text}>Login</Text>
        </Pressable>
         </View>
@@ -15,6 +20,10 @@ function AcceuilScreen() {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     pressable: {
         color: 'blue',
@@ -22,8 +31,21 @@ const styles = StyleSheet.create({
         padding: 6,
         margin: 4,
         borderRadius: 10
-      }
+    },
+    logo: {
+        width: 70,
+        height: 62
+    },
+    title: {
+        borderRadius: 10,
+        color: 'green',
+        fontFamily: 'Serif'
+    },
+    pressable_text: {
+        fontSize:15,
+        alignItems: 'center',
+    }
     
 })
 
-export default AcceuilScreen;
+export default AccueilScreen;
