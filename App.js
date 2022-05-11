09e801411c47a9dceaf32353bd136abcb05e8d89
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-05-11 15:13:13 -0400

    LPR

--- App.js
+++ App.js
@@ -20,18 +20,18 @@ const Stack = createNativeStackNavigator();
 function init_tab(db){
   db.execute("drop table if exists produits;");
   db.execute("create table produits (id, nom, prix, image);");
-  db.execute("drop table if exists connexion;");
-  db.execute("create table connexion (id , usager, motdepasse, admin);");
+  db.execute("drop table if exists connexions;");
+  db.execute("create table connexions (id , usager, motdepasse, admin);");
 
   db.execute("insert into produits values (1, 'Pain 800g' , 10 , image.jpg);");
-  db.execute("insert into Connexion values (1 , 'LPR' , '123456' , 1);");
+  db.execute("insert into connexions values (1 , 'LPR' , '123456' , 1);");
 }
 init_tab(db);
 
 
 
 export default function App() {
-  const [connexions , setConnexion] = useState([...connexions]);
+  const [connexions , setConnexion] = useState([]);
   const [produits, setProduits] = useState([]);
   const [userConnected , setUserConnected] = useState(false);
   const [nomUser , setUser] = useState();
@@ -60,15 +60,15 @@ export default function App() {
 }
 
 const AllConnexions = ({navigation}) => {
-  const [connexions , setConnexion] = useState([...connexions]);
+  const [connexions , setConnexion] = useState([]);
   const [erreur , setErreur] = useState();
-  db.execute("select * from connexions;")
+  db.execute("select usager , admin from connexions;")
   .then((resultSet) => {
       setConnexion(resultSet.rows)
   }).catch((m)=>{  setErreur("Erreur exec Select " + m);})
   return  <View  style={styles.container}>
     <Text>{connexions != null ? 'yo' : null}</Text>
-  {connexions.map((n)=><PressableLogin onPress={() => navigation.navigate("Acceuil", {userID: n.userID, name: n.nom, admin: n.admin})} user={n.user} motdepasse={n.motdepasse} flag={n.admin}>{n.user}{n.admin}</PressableLogin>)}
+  {connexions.map((n)=><PressableLogin onPress={() => navigation.navigate("Acceuil", { user: n.usager, flag: n.admin})} user={n.usager} flag={n.admin}></PressableLogin>)}
   </View>
 }
 const styles = StyleSheet.create({
--- Components/PressableLogin.js
+++ Components/PressableLogin.js
@@ -1,19 +1,69 @@
 import React from 'react';
-import { View, StyleSheet , Pressable } from 'react-native';
-
+import { View, StyleSheet , Pressable ,Text} from 'react-native';
+import { useState } from 'react';
 function PressableLogin(props) {
     const [isPressed, setIsPressed] = useState(false);
      return (<Pressable style={isPressed ? styles.appuye : styles.pressable}
        onPressIn={() => setIsPressed(true)} onPressOut={() => setIsPressed(false)}>
-         <Text style={isPressed ? styles.texteAppuye : styles.texteNormal}>{user}{flag}</Text>
-         {setUser(user)}{setUserConnected(true)} {flag == 1?setAdminConnecter(true):setAdminConnecter(false)}
+         <Text style={isPressed ? styles.texteAppuye : styles.texteNormal}>{props.user}{props.flag}</Text>
+         
      </Pressable>
      )
 }
-
+/* {setUser(props.user)}{setUserConnected(true)} {flag == 1?setAdminConnecter(true):setAdminConnecter(false)}*/
 const styles = StyleSheet.create({
-    container: {
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 35,
+    color: 'white',
+    backgroundColor: '#38f'
+  },
+  ligneCroisiere: {
+    fontSize: 30,
+    color: 'white',
+    padding: 7,
+    margin: 7,
+    backgroundColor: 'blue'
+  },
+  croisiere: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'blue',
+    padding: 4,
+    margin: 4
+  },
+  pressable: {
+    color: 'blue',
+    backgroundColor: '#2be',
+    padding: 6,
+    margin: 4,
+    borderRadius: 10
+  },
+  pressable_text:{
+    fontSize:15,
+
+    alignItems: 'center',
+  },
+
+  appuye: {
+    backgroundColor: '#26e',
+    padding: 6,
+    margin: 4,
+    borderRadius: 10
+  },
+  texteAppuye: {
+    color: 'lightblue',
+    fontWeight: 'bold'
+  },
+  texteNormal: {
+    color: 'blue',
+    fontWeight: 'bold'
+  }
 })
 
 export default PressableLogin;
